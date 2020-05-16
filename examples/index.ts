01bb25ec0b6907a9e4eb482f07585f4e3cebe95c
nusendra <admin@nusendra.com>
2020-05-17 03:17:37 +0700

    added example file

---
+++ examples/index.ts
@@ -0,0 +1,4 @@
+import format from "./../mod.ts"
+
+const numberFormat = format("#,##0.00", 123456789.123);
+console.log(numberFormat)
--- index.ts
+++
@@ -1,4 +0,0 @@
-import format from "./formatter.ts"
-
-const b = format("#,##0.00", 123456789.123);
-console.log(b)
